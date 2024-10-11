a9c4465d0932d61a137ee6e3c6949cdfdf18725f
xurich-xulaco <82631735+xurich-xulaco@users.noreply.github.com>
2024-10-10 20:37:57 -0600

    AgarrarCajas
    Esto deberían de ser cables para hacer un rompecabezas, pero el placeholder es mover cajas

---
+++ RobotChambaMalotetronSkibidiChadΣ/Assets/Scripts/Player/GrabObjects.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GrabObjects : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform grabPoint;
+
+    [SerializeField]
+    private Transform rayPoint;
+    [SerializeField]
+    private float rayDistance;
+
+    private GameObject grabbedObject;
+    private int layerIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerIndex = LayerMask.NameToLayer("Agarrable");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
+
+        if (hitInfo.collider!=null && hitInfo.collider.gameObject.layer == layerIndex)
+        {
+            //Agarrar objeto
+            if (Input.GetKeyDown(KeyCode.R) && grabbedObject == null)
+            {
+                grabbedObject = hitInfo.collider.gameObject;
+                grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                grabbedObject.transform.position = grabPoint.position;
+                grabbedObject.transform.SetParent(transform);
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                grabbedObject.transform.SetParent(null);
+                grabbedObject = null;
+            }
+        }
+
+        Debug.DrawRay(rayPoint.position, transform.right * rayDistance);
+    }
+}
