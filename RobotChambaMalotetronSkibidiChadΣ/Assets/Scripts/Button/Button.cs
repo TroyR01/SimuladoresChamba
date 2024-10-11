bab2b98678abb10ff0ac93803e97bc82f9340e46
unknown <andrago2025@hotmail.com>
2024-10-10 23:23:25 -0600

    added animation to buttons and to doors

--- RobotChambaMalotetronSkibidiChadΣ/Assets/Scripts/Button/Button.cs
+++ RobotChambaMalotetronSkibidiChadΣ/Assets/Scripts/Button/Button.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     public GameObject door;
+    public Animator animator;
 
     // Asegúrate de que el objeto del botón tiene un Collider2D con la opción "Is Trigger" marcada
 
@@ -14,7 +15,9 @@ public class Button : MonoBehaviour
         
         if(collision.CompareTag("Player"))
         {
-            door.SetActive(false); 
+            animator.SetBool("Open",true);
+            door.GetComponent<Animator>().SetTrigger("Open");
+            door.GetComponent<BoxCollider2D>().enabled=false; 
            
         }
     }
